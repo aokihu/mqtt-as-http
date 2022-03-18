24d4c3642b1858e37fefa98c410c17fa6cf250f8
aokihu <aokihu@gmail.com>
2022-03-18 16:53:02 +0800

    update: update client code

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,44 @@
-import BaseHttp, { RequestMessage,  ResponseMessage } from './base'
-import {fmid} from 'utils'
+import BaseHttp from './base'
+import {fmid} from './utils'
 import { MqttClient } from 'mqtt';
 
+
+/**
+ * @package mqtt-as-http
+ * @module client
+ * @class Client
+ * @author aokihu <aokihu@gmail.com>
+ * @version $VERSION
+ * @license MIT
+ */
 export default class Client extends BaseHttp {
-  static timeout = 500; // Timeout for request wait
-  static lengthOfNanoid = 8;
-  private _queue:RequestQueueItem[] = Array<RequestQueueItem>();
+  
+  /* ---------------------------------- */
+  /*      Static constant variables     */
+  /* ---------------------------------- */
+  
+  /* Timeout for request wait */
+  static TIMEOUT = 30000; 
+  
+  static RESPONSE_REGEXP = /^(\S+)\/@_mqtt_as_http_\/res\/(GET|POST|PUT|DELETE)\/(\S+)$/
+
+  /* ---------------------------------- */
+  /*          Private variables         */
+  /* ---------------------------------- */
+  
+  private _queue: RequestQueueItem[] = Array<RequestQueueItem>();
   private _nanoid:Function;
 
+  /**
+   * @constructor
+   * @param mqtt MQTT client adpater object
+   */
   constructor(mqtt:MqttClient) {
     super(mqtt);
     try {
-      this._nanoid = () => fmid(Client.lengthOfNanoid);
+      this._nanoid = () => fmid(8);
       this._mqtt?.on('message', this._handleResponse.bind(this));
-      setInterval(this._clearExpireItem.bind(this), Client.timeout * 2);
+      setInterval(this._clearExpireItem.bind(this), Client.TIMEOUT * 2);
     } catch (error) {
       throw error;
     }
@@ -26,42 +51,48 @@ export default class Client extends BaseHttp {
    * @param body Attatch payload data
    */
   public request<T=any>(method: RequestMethods, topic:string, body: any): Promise<T> {
-    const uuid = this._nanoid();
-    const expires = Date.now() + Client.timeout;
-    const responseTopic = this._makeResponseTopic(topic,method, uuid);
-    const requestTopic = this._makeRequestTopic(topic, method, uuid);
+    const expires = Date.now() + Client.TIMEOUT;
+    const [uuid, requestTopic, responseTopic] = this.generateFullTopic(method, topic)
 
     try {
-      this._mqtt?.subscribe(responseTopic);
       const payload = this._makeRequestMessage(method, body);
-      this._mqtt?.publish(requestTopic, JSON.stringify(payload), {qos:0});
+      this._mqtt?.subscribe(responseTopic);
+      this._mqtt?.publish(requestTopic, JSON.stringify(payload), {qos:2});
     }
     catch(error) {throw error}
     finally {
-      return new Promise((resolve, reject) => {
+      return new Promise<any>((resolve, reject) => {
         const item: RequestQueueItem = {uuid, expires, reject, resolve, topic:responseTopic};
         this._queue.push(item);
       });
     }
   }
 
-  public get(topic:string, body?: any): Promise<any> {return this.request(RequestMethods.GET, topic, body);}
-  public post(topic:string, body: any): Promise<any> {return this.request(RequestMethods.POST, topic, body)}
-  public put(topic:string, body: any): Promise<any> {return this.request(RequestMethods.PUT, topic, body)}
-  public del(topic:string, body?: any): Promise<any> {return this.request(RequestMethods.DELETE, topic, body)}
+  public get(topic:string, body?: any): Promise<any> {return this.request("GET", topic, body);}
+  public post(topic:string, body: any): Promise<any> {return this.request("POST", topic, body)}
+  public put(topic:string, body: any): Promise<any> {return this.request("PUT", topic, body)}
+  public del(topic:string, body?: any): Promise<any> {return this.request("DELETE", topic, body)}
+
+
+  /* ---------------------------------- */
+  /*           Private methods          */
+  /* ---------------------------------- */
 
   private _handleResponse(topic:string, payload:Buffer) {
-    const responseMessage:ResponseMessage = JSON.parse(payload.toString()) as ResponseMessage;
-    const _segment = topic.split('/');
-    const _uuid = _segment[_segment.length - 1];
-    const idx = this._queue.findIndex( it => it.uuid === _uuid);
-
-    if(idx >= 0) {
-      const [{expires, resolve, reject}] = this._queue.splice(idx, 1); // remove this request from queue
-      this._mqtt?.unsubscribe(topic);
-      expires > Date.now() ? resolve(responseMessage) : reject('expires is timeout');
-    } else {
-      throw(new Error("event is not register"));
+    const result = Client.RESPONSE_REGEXP.exec(topic);
+    
+    if(result) {
+      const _uuid = result[3]
+      const responseMessage:ResponseMessage = JSON.parse(payload.toString()) as ResponseMessage;
+      const idx = this._queue.findIndex( it => it.uuid === _uuid);
+      
+      if(idx >= 0) {
+        const [{expires, resolve, reject, topic:_topic}] = this._queue.splice(idx, 1); // remove this request from queue
+        this._mqtt?.unsubscribe(topic);
+        expires > Date.now() ? resolve(responseMessage) : reject('expires is timeout');
+      } else {
+        throw(new Error("event is not register"));
+      }
     }
   }
 
@@ -80,15 +111,5 @@ export default class Client extends BaseHttp {
 
   private _makeRequestMessage(method:RequestMethods, data: any):RequestMessage {
     return {data}
-  }
-
-  private _makeResponseTopic(topic:string, method:RequestMethods, uuid:string):string {
-    const slash = topic.endsWith("/") ? "" : "/";
-    return `${topic}${slash}@response/${method}/${uuid}`;
-  }
-
-  private _makeRequestTopic(topic:string, method:RequestMethods, uuid:string):string {
-    const slash = topic.endsWith("/") ? "" : "/";
-    return `${topic}${slash}@request/${method}/${uuid}`;
   }
 }
