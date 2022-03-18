7dd247902c31ede3440697899384c9f95d132f07
aokihu <aokihu@gmail.com>
2022-03-18 16:53:21 +0800

    add: new test case for long topic

--- test/test.js
+++ test/test.js
@@ -1,24 +1,58 @@
 const Mqtt = require('mqtt');
-const {Client} = require('../dist/index')
+const {Client, Server} = require('../dist/index')
 const expect = require('chai').expect;
 
-const mqtt = Mqtt.connect("mqtt://127.0.0.1")
-const client = new Client(mqtt);
+const forkMqtt = () => Mqtt.connect("mqtt://127.0.0.1")
+
+const mqtt_client = forkMqtt();
+const mqtt_server = forkMqtt();
+
+const client = new Client(mqtt_client);
+const server = new Server(mqtt_server)
 
 describe('Mqtt GET method test', function() {
 
-  after(function() {
+  this.timeout(1000)
 
-    mqtt.end();
+  this.afterAll(function() {
+    mqtt_client.end();
+    mqtt_server.end();
     process.exit(0);
-
   })
+  
+  /* Set server */
+  this.beforeAll((done) => {
+    console.log('setup server')
+    
+    let param = 0;
+    server.get("hello", () => 'world')
+    server.get("hello/world", () => "peace")
+    server.get("param", () => param)
+    server.post("param", (topic, data) => {
+      param = data
+    })
+    server.del("param", (topic, data) => {
+      param = 0
+    })
 
+    console.log('server ready...')
+    done();
+  }) 
+
+  /* Client test cases */
   it("should return world", function(done){
     client.get("hello").then(response => {
       const {data} = response;
       expect(data).to.equal('world');
       done();
+    })
+  })
+
+  it("longer topic, should return world", (done) => {
+    client.get("hello/world").then(response => {
+      const {data} = response;
+      expect(data).to.equal('peace');
+      done()
     })
   })
 
