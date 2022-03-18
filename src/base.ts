0a52a42f7f9ca8d19fe6f02c3b5b4e738474398e
aokihu <aokihu@gmail.com>
2022-03-18 16:52:37 +0800

    del: delete some no use code

--- src/base.ts
+++ src/base.ts
@@ -1,22 +1,65 @@
+/**
+ * @package mqtt-as-http
+ * @module base
+ * @author aokihu <aokihu@gmail.com>
+ * @version $VERSION
+ * @license MIT
+ */
+
+
 import { EventEmitter } from "events";
-import { MqttClient } from "mqtt";
+import type { MqttClient } from "mqtt";
+import {fmid} from './utils'
 
-export type ResponseMessage = {
-  time: number,
-  data: any,
-}
-
-
-export type RequestMessage = {
-  data: any
-}
-
+/**
+ * Serve and client base class
+ */
 export default class BaseHttp extends EventEmitter {
-  protected _mqtt:MqttClient | undefined;
+  
+  /* ---------------------------------- */
+  /*      Static constant variables     */
+  /* ---------------------------------- */
 
-  constructor(mqtt:MqttClient) {
+  static FULL_TOPIC_PARTTEN = /(\w)+\/@_mqtt_as_http\/[res|req]\/[GET|POST|PUT|DELETE]\/(\w+)/
+
+  /* ---------------------------------- */
+  /*         Protected variables        */
+  /* ---------------------------------- */
+
+  protected _mqtt: MqttClient | undefined;
+
+  /* ---------------------------------- */
+  /*          Private functions         */
+  /* ---------------------------------- */
+
+  /**
+   * @constructor
+   * @param mqtt MQTT client adapter object
+   */
+  constructor(mqtt: MqttClient) {
     super();
     this._mqtt = mqtt;
   }
 
+
+  /* ---------------------------------- */
+  /*         Protected functions        */
+  /* ---------------------------------- */
+
+  /**
+   * Generate full topic for response or request
+   * @param method the method of request
+   * @param rawTopic raw topic
+   * @returns [uuid, requestTopic, responseTopic] 
+   * - uuid: The topic id
+   * - requestTopic: Request full topic string
+   * - responseTopic: Response full topic string
+   */
+  protected generateFullTopic( method: RequestMethods, rawTopic: string): [string, string, string]{
+    const slash = rawTopic.endsWith("/") ? "" : "/"
+    const id = fmid(8);
+    return [id, `${rawTopic}${slash}@_mqtt_as_http_/req/${method}/${id}`, `${rawTopic}${slash}@_mqtt_as_http_/res/${method}/${id}`]
+  }
 }
+
+
